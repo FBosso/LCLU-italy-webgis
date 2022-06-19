4030af306e892a7e4a792dd3d354145bc5a126db
FBosso <francesco.bosso@mail.polimi.it>
2022-06-19 10:21:26 +0200

    first setup main pages

---
+++ pages/contatti.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container mt-5">
+        <TopPageGeneric :name="topData.name" :description="topData.description" :tags="topData.tags" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ContattiPage",
+        data(){
+            return{
+                topData:{
+                    name: "Contatti",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, soluta deserunt dolor asperiores amet aut doloremque illum similique, rem voluptates temporibus animi excepturi. Fugit alias quae inventore asperiores? Voluptas, laudantium.",
+                    tags: ["Mail", "Phone", "Linkedin"]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/risorse.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container mt-5">
+        <TopPageGeneric :name="topData.name" :description="topData.description" :tags="topData.tags" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "RisorsePage",
+        data(){
+            return{
+                topData:{
+                    name: "Risorse",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, soluta deserunt dolor asperiores amet aut doloremque illum similique, rem voluptates temporibus animi excepturi. Fugit alias quae inventore asperiores? Voluptas, laudantium.",
+                    tags: ["DatiGeospaziali", "Regioni", "LandCover", "LandUse"]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
