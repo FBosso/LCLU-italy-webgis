0905727ecd246516fc477071585d59b6e000bc10
FBosso <francesco.bosso@mail.polimi.it>
2022-06-20 10:35:59 +0200

    contact page finshed

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
       { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;900&display=swap" }
     ],
