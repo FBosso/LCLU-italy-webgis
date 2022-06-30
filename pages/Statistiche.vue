bacb467fa2ea707c0164243d370c74c6a6e2fc57
FBosso <francesco.bosso@mail.polimi.it>
2022-06-30 21:40:35 +0200

    statistiche

--- pages/Statistiche.vue
+++ pages/Statistiche.vue
@@ -1,43 +1,104 @@
 <template>
   <div class="container mt-5">
     <div class="row mb-3">
-    <TopPageGeneric
-      :name="topData.name"
-      :description="topData.description"
-      :tags="topData.tags"
-    />
+      <TopPageGeneric
+        :name="topData.name"
+        :description="topData.description"
+        :tags="topData.tags"
+      />
     </div>
     <div class="subtitle">Risorse</div>
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Materiale</th>
+            <th scope="col">link</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">PDF di presentazione del lavoro</th>
+            <td scope="row">
+              <a class="link" href="https://shorturl.at/orJOQ"
+                >https://shorturl.at/orJOQ</a
+              >
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Github repo Jupyter Notebook</th>
+            <td scope="row">
+              <a
+                class="link"
+                href="https://github.com/FBosso/LandCover_LandUse_Italy"
+                >https://github.com/FBosso/LandCover_LandUse_Italy</a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Materiale</th>
-          <th scope="col">link</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">PDF di presentazione del lavoro</th>
-          <td scope="row"> <a class="link" href="https://shorturl.at/orJOQ">https://shorturl.at/orJOQ</a> </td>
-        </tr>
-        <tr>
-          <th scope="row">Github repo Jupyter Notebook</th>
-          <td scope="row"> <a class="link" href="https://github.com/FBosso/LandCover_LandUse_Italy">https://github.com/FBosso/LandCover_LandUse_Italy</a> </td>
-        </tr>
-        
-      </tbody>
-    </table>
-
-    <div class="subtitle mb-3">Jupyter notebook</div>
-    <iframe
-    title="Jupyter notebook analisi dati"
-      src="https://fbosso.github.io/CNITA"
-      width="100%"
-      height="2000px"
-      class="notebook"
-    >
-    </iframe>
+    <div class="subtitle mb-3">Overview analisi</div>
+    <div class="snippet">
+      Dopo il censimento di tutti i dati citati all'interno dell'inventario di
+      ISPRA, tenendo in considerazione molteplici aspetti (presenza/assenza di:
+      possibilità di download dei dati, metadati, richieste WFS, richieste WMS,
+      possibilità di connessioni ad ArcGIS REST SERVER, ecc ), è stata
+      sviluppata una analisi quantitativa preliminare per valutare lo stato di
+      fatto dei dati di Uso e Copertura del suolo sotto due aspetti
+      fondamentali:
+      <b>Reperibilità delle informazioni</b> e <b>Accessibilità del dato</b>. I
+      risultati sono sintetizzati all'intenro del seguente grafico che presenta
+      sull'asse delle "x" l'indice di <u>reperibilità delle informazioni</u> e
+      sull'asse delle "y" l'indice di <u>accessibilità del dato</u> che possono
+      assumere valori compresi tra 0 (minimo) ed 1 (massimo):
+    </div>
+    <div class="center">
+      <figure>
+        <iframe
+          src="https://fbosso.github.io/Bubbles_Regions"
+          width="950px"
+          height="750px"
+          class="mt-5"
+        >
+          <p>prova</p>
+        </iframe>
+        <figcaption class="caption">
+          <b>Figura 1</b> - Ogni cerchio nel grafico rappresenta una regione e
+          il diametro di ciascun cerchio è proporzionale al numero di risorse
+          considerate per il calcolo della statistica riferita a quella regione
+        </figcaption>
+      </figure>
+    </div>
+    <div class="snippet mt-5">
+      Come informazione qualitativa aggiuntiva si è voluto applicare un semplice
+      algoritmo di clustering per identificare dei gruppi di regioni aventi
+      necessità simili sotto il punto di vista di miglioramento dei parametri
+      considerati. Questo approccio, adottato nuovamente quando il campione dei
+      dati analizzati aumenterà anche per quelle regioni che, ad ora, sono state
+      analizzate con un numero limitato di dati, consentirà di pianificare delle strategie
+      rafforzative aderenti alle esigenze di ciascun cluster.
+    </div>
+    <div class="center">
+      <figure>
+        <iframe
+          src="https://fbosso.github.io/Bubbles_Regions_clusters"
+          width="950px"
+          height="750px"
+          class="mt-5"
+        >
+          <p>prova</p>
+        </iframe>
+        <figcaption class="caption">
+          <b>Figura 2</b> - Ogni cerchio nel grafico rappresenta una regione e
+          il diametro di ciascun cerchio è proporzionale al numero di risorse
+          considerate per il calcolo della statistica riferita a quella regione.
+          I colori dei cerchi rappresentano i clusters identificati
+          dall'agoritmo di clustering
+        </figcaption>
+      </figure>
+    </div>
   </div>
 </template>
 
@@ -66,7 +127,7 @@ export default {
             'wfs',
             'wms',
             'arcgis',
-            'analisi dati'
+            'analisi dati',
           ],
         },
       ],
@@ -90,7 +151,18 @@ export default {
   border: solid 2px rgb(169, 169, 169);
   border-radius: 10px;
 }
-.link{
-    color: rgb(70, 70, 222);
+.link {
+  color: rgb(70, 70, 222);
+}
+.center {
+  text-align: center;
+}
+iframe {
+  align-items: center;
+  text-align: center;
+}
+.caption {
+  color: grey;
+  text-align: center;
 }
 </style>
