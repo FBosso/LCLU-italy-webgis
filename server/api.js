88c5bed29c2b9b91fff8c1c418022fad878d6bad
FBosso <francesco.bosso@mail.polimi.it>
2022-07-05 21:17:18 +0200

    heroku deploy better scores

--- server/api.js
+++ server/api.js
@@ -11,7 +11,7 @@ const { Client } = require('@elastic/elasticsearch');
 
 
 /* DEV */
-const database = new Sequelize('postgres://postgres:postgres@localhost:5432/geo-nuxt', {
+/* const database = new Sequelize('postgres://postgres:postgres@localhost:5432/geo-nuxt', {
     logging: false //Set to true to log DB actions
 })
 
@@ -22,11 +22,11 @@ const client = new Client({
         username: 'elastic',
         password: 'UuraDFRJ6iedIEhgqNcaEdbb'
     }
-});
+}); */
 
 
 /* PROD */
-/* const pg = require('pg')
+const pg = require('pg')
 pg.defaults.ssl = true
 const database = new Sequelize(process.env.DATABASE_URL, {
    ssl: true,
@@ -40,7 +40,7 @@ const client = new Client({
         username: process.env.ELASTIC_USER,
         password: process.env.ELASTIC_PASSWORD
     }
-}); */
+});
 
 
 /* CORS NON è PIù NECESSARIO IN QUANTO SIA APPLICATION 
